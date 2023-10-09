0be9ba67ec9802554368c36bbdecc7e79a5515f8
Dmitry Khrunin <thesunshineofficial@gmail.com>
2023-10-09 19:43:37 +0300

    Update profile page

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -9,11 +9,11 @@ interface Props {
 
 const Profile = ({ userResponse, roleResponse }: Props) => {
   return (
-    <>
+    <div className="mb-6">
       <h1 className="mb-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">Hello, <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">{userResponse.username}</span>!</h1>
       <p className="text-gray-500 text-xl">Name: <span className="font-semibold text-gray-900 underline decoration-green-500">{userResponse.firstName} {userResponse.lastName}</span></p>
       <p className="text-gray-500 text-xl">Role: <span className="font-semibold text-gray-900 underline decoration-green-500">{roleResponse.name}</span></p>
-    </>
+    </div>
   );
 };
 
--- src/Pages/ProfilePage/ProfilePage.tsx
+++ src/Pages/ProfilePage/ProfilePage.tsx
@@ -1,13 +1,56 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Navigate, useOutletContext} from "react-router-dom";
 import Profile from "../../Components/Profile/Profile";
 import {AppContext} from "../../App";
+import {PostPageResponse} from "../../Api/Interfaces/post";
+import {CommentPageResponse} from "../../Api/Interfaces/comment";
+import {getAllComments, getAllPosts} from "../../Api/api";
+import PageHeader from "../../Components/PageHeader/PageHeader";
+import ErrorMessage from "../../Components/ErrorMessage/ErrorMessage";
+import PostCardList from "../../Components/PostCardList/PostCardList";
+import CommentCardList from "../../Components/CommentCardList/CommentCardList";
 
 interface Props {
 }
 
 const ProfilePage = (props: Props) => {
   const { user, role } = useOutletContext<AppContext>();
+  const [postPageResponse, setPostPageResponse] = useState<PostPageResponse | null>(null);
+  const [commentPageResponse, setCommentPageResponse] = useState<CommentPageResponse | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getAllPostsInit = async () => {
+      const response = await getAllPosts(0, 10, user?.id);
+
+      if (typeof response !== "string") {
+        if (response.status === 200) {
+          setPostPageResponse(response.data);
+        } else {
+          setError(response.data.message);
+        }
+      } else {
+        setError(response);
+      }
+    };
+
+    const getAllCommentsInit = async () => {
+      const response = await getAllComments(0, 10, user?.id);
+
+      if (typeof response !== "string") {
+        if (response.status === 200) {
+          setCommentPageResponse(response.data);
+        } else {
+          setError(response.data.message);
+        }
+      } else {
+        setError(response);
+      }
+    };
+
+    getAllPostsInit();
+    getAllCommentsInit();
+  }, []);
   
   if (user === null)
     return <Navigate replace to="/login" />
@@ -15,6 +58,11 @@ const ProfilePage = (props: Props) => {
   return (
     <>
       { user && role && <Profile userResponse={user} roleResponse={role} /> }
+      <PageHeader>My posts</PageHeader>
+      { error && <ErrorMessage>{error}</ErrorMessage> }
+      { postPageResponse && <PostCardList pageResponse={postPageResponse} currentUser={user} role={role} /> }
+      <PageHeader>My comments</PageHeader>
+      { commentPageResponse && <CommentCardList pageResponse={commentPageResponse} currentUser={user} role={role} /> }
     </>
   );
 };
