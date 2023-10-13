fa2c1295c7c2537dde3a831f1b949d822827119e
Dmitry Khrunin <thesunshineofficial@gmail.com>
2023-10-13 09:12:45 +0300

    Add keys to pagination

--- src/Components/Pagination/Pagination.tsx
+++ src/Components/Pagination/Pagination.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PaginationButton from "../PaginationButton/PaginationButton";
+import {v4 as uuidv4} from "uuid";
 
 interface Props {
   pageNumber: number;
@@ -27,15 +28,15 @@ const Pagination = ({ pageNumber, totalPages, last, setPageNumber }: Props) => {
       <ul className="inline-flex -space-x-px text-base h-10">
         { pageNumber !== 0 &&
             <li>
-                <button type="button" onClick={onPreviousClick} className="flex items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700">Previous</button>
+                <button type="button" onClick={onPreviousClick} key={uuidv4()} className="flex items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700">Previous</button>
             </li>
         }
         { numbers.map((n) => {
-          return <PaginationButton number={n} setPageNumber={setPageNumber} isCurrent={pageNumber + 1 === n} />
+          return <PaginationButton key={uuidv4()} number={n} setPageNumber={setPageNumber} isCurrent={pageNumber + 1 === n} />
         })}
         { !last &&
             <li>
-                <button type="button" onClick={onNextClick} className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700">Next</button>
+                <button type="button" onClick={onNextClick} key={uuidv4()} className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700">Next</button>
             </li>
         }
       </ul>
